b8f3b962abbd122b4863cdf214b44bdaef7f327f
JhonatanNobrega <jhonatan_nobrega@hotmail.com>
2021-03-31 23:49:32 -0300

    Feito alguns processo de programacao defensiva e iniciamos a criacao do controller para negociacao

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,18 +1,55 @@
 
 class Negociacao {
 
-    /*
-    * No contrutor que definimos as propriedades.
-    * Toda class com contrutor só pode chamar com new Negociacao.
-    * this sempre aponta para a execução do momento ou seja essa class.
+  /*
+  * No contrutor que definimos as propriedades.
+  * Toda class com contrutor só pode chamar com new Negociacao.
+  * this sempre aponta para a execução do momento ou seja essa class.
+  * */
+  constructor( quantidade = 1, valor = 0.00 )
+  {
+    // Definindo as propriedades de Negociacao
+    this._data = new Date();
+    this._quantidade = quantidade;
+    this._valor = valor;
+
+    /* Após criado congela os valores dessa class
+    * Podemos ter problema para encapsular desta forma porque freezer é superficie fina.
+    * Freezer não congela outro objetos dentro dessa class como por exemplo o uso de new Date().
     * */
-    constructor()
-    {
+    Object.freeze(this);
+  }
 
-        // Definindo as propriedades de Negociacao
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.00;
-    }
+  /*
+  * Todas as funções dentro de class chamamos de método
+  * Quando criarmos função que devolve valores podemos user o get ou set para definir valor.
+  * 
+  * */
+  get volume() 
+  {
+    return this._quantidade * this._valor;
+  }
 
+  get data()
+  {
+    // Retornando uma cópia da data para evitar a manipulação de dada por ações externas
+    return new Date( this._data.getTime() );
+  }
+  get quantidade()
+  {
+    return this._quantidade;
+  }
+  get valor()
+  {
+    return this._valor;
+  }
+
+  /*
+  * No curso entendi que a classe pode ser facilmente manipulada. Não tem método privado como no php.
+  * Então temos que fazer programação defenciva.
+  * 
+  * Uma coisa que podemos fazer nesse caso é uma convenção. 
+  * Se na class esse valor não pode ser auterado então definimos antes do nome um undeline. Exemp. _funcao _variavel
+  * 
+  * */
 }
